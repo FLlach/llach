acb04e36932ef8aeef9f472a4f612a57a0b35970
Felipe Llach Richmond <f.ll.richmond@gmail.com>
2024-04-05 18:52:58 -0300

    commit de prueba

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-const {src, dest, watch} = require("gulp");
+const {task, src, dest, watch} = require("gulp");
 const sass = require("gulp-sass")(require('sass'));
-const plumber = require( "gulp-plumber" );
-const webp = require("gulp-webp");
+const plumber = require( "gulp-plumber");
+//const webp = require("gulp-webp");
+const git = require("gulp-git");
 
 function css(done){
     src("src/scss/**/*.scss")//Identificar archivo
@@ -22,5 +23,44 @@ function cimg(done){
     src("")
     done()
 }
+
+function gitinit(done){
+    task('init', function(){
+        git.init(function (err) {
+        if (err) throw err;
+        });
+    });
+    done()
+    }
+
+function gitpull(done){
+    // Task to pull changes from the remote repository
+    task('git-pull', function() {
+        return git.pull('origin', 'master', function(err) {
+            if (err) throw err;
+        });
+    });
+    done()
+}
+
+function addremote(done){
+    git.addRemote('origin', 'https://github.com/FLlach/llach')
+    done()
+
+}
+
+function gitpush(done){
+    // Task to push changes to the remote repository
+    src('./*')
+        .pipe(git.add())
+        .pipe(git.commit("auto commit"))
+        .pipe(git.push('origin', 'master'));
+
+    done()
+}
+
+exports.gitinit = gitinit
+exports.gitpush =  gitpush
+exports.gitpull =  gitpull
 exports.css= css
 exports.dev= dev
